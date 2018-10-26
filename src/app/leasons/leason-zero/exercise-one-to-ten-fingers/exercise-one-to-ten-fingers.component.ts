8f7d60a95549386690b539fb13c0719e9873282c
Daniel Miles <danielmiles89@gmail.com>
2018-10-26 18:48:07 +0300

    mare images working

--- src/app/leasons/leason-zero/exercise-one-to-ten-fingers/exercise-one-to-ten-fingers.component.ts
+++ src/app/leasons/leason-zero/exercise-one-to-ten-fingers/exercise-one-to-ten-fingers.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import * as data from './strings.json';
 
+interface exercise {
+  question: string;
+  answer: string;
+  hint: boolean;
+  statTime: Date;
+  endTime?: Date;
+}
+
 @Component({
   selector: 'app-exercise-one-to-ten-fingers',
   templateUrl: './exercise-one-to-ten-fingers.component.html',
@@ -8,11 +16,23 @@ import * as data from './strings.json';
 })
 export class ExerciseOneToTenFingersComponent implements OnInit {
   imagesSrc = (<any>data).images;
+  exercise: exercise;
   constructor() { }
 
 
   ngOnInit() {
-    console.log(this.imagesSrc.finger_0);
+    console.log(this.imagesSrc)
+    this.exercise = this.createNewExercise();
   }
-
+  createNewExercise(): exercise {
+    const num = Math.floor(Math.random() * 9) + 1
+    let exercise = {
+      question: this.imagesSrc[`finger_${num}`],
+      answer: this.imagesSrc[`finger_${num}`],
+      hint: false,
+      statTime: new Date()
+    }
+    console.log(exercise)
+    return exercise
+  }
 }
