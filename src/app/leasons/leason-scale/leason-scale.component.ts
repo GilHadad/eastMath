1ac18179e731f98f90eb318d3e3e2a64940f1245
Daniel Miles <danielmiles89@gmail.com>
2018-11-04 19:10:08 +0200

    styling

--- src/app/leasons/leason-scale/leason-scale.component.ts
+++ src/app/leasons/leason-scale/leason-scale.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import * as data from './strings.json';
+import {interval} from 'rxjs';
+
 
 interface scale {
   scalesFace: string;
   lives: number;//a nubmer beween 1 to 10
+  scaleRank: number;//a nubmer beween 1 to 5
 }
 
 @Component({
@@ -20,6 +23,8 @@ export class LeasonScaleComponent implements OnInit {
 
   ngOnInit() {
     this.scale = this.createNewScale();
+    const source = interval(2000);
+    source.subscribe(() => this.reduceScalesLives(1))
   }
 
   createNewScale(): scale {
@@ -28,17 +33,39 @@ export class LeasonScaleComponent implements OnInit {
     let scale = {
       lives: lives,
       scalesFace: this.imagesSrc[`Face_${scaleRank}`],
+      scaleRank: scaleRank
     }
     return scale
   }
 
   reduceScalesLives(num) {
-    this.scale.lives = this.scale.lives - num;
+    if(this.scale.lives > 0){
+      const currentlives = this.scale.lives - num;
+      this.scale.lives = currentlives;
+      this.updateScale(currentlives);
+      console.log('reduceScalesLives by 1');
+    }
+
   }
 
   getScaleRank(lives) {
     const rank = Math.ceil(lives / 2);//there can be 10 lives and only 5 moods
     return rank
   }
+
+  updateScale(lives) {
+    this.scale.lives = lives;
+    const scaleRank = this.getScaleRank(lives);
+    this.scale.scaleRank = scaleRank
+    this.updateScaleImage(scaleRank);
+  }
+
+  updateScaleImage(scaleRank) {
+    if(scaleRank > 0){
+      this.scale.scalesFace = this.imagesSrc[`Face_${scaleRank}`];
+    }
+  }
+
+
 
 }
